73f589f02b42767e077b7d114fd278eb90124360
sambuitr48 <buitragoosoriosamuel@gmail.com>
2024-06-09 00:54:24 -0500

    1.5 se deben corregir errores

--- src/main/java/co/cue/edu/miniproject/demo/controllers/UserController.java
+++ src/main/java/co/cue/edu/miniproject/demo/controllers/UserController.java
@@ -1,4 +1,37 @@
 package co.cue.edu.miniproject.demo.controllers;
 
+import co.cue.edu.miniproject.demo.dtos.UserDTO;
+import co.cue.edu.miniproject.demo.models.User;
+import co.cue.edu.miniproject.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/add-user")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @DeleteMapping(value = "/delete-user/{user_id}")
+    public void removeUser(@PathVariable Long user_id) {
+        userService.removeUser(user_id);
+    }
+
+    @GetMapping(value = "/get-users")
+    public List<UserDTO> getAllUsers() {
+      return userService.getUser();
+    }
+
+    @GetMapping(value = "/get-user-byid/{user_id}")
+    public void userById(@PathVariable Long user_id){
+        userService.findById(user_id);
+    }
 }
--- src/main/java/co/cue/edu/miniproject/demo/controllers/VehicleController.java
+++ src/main/java/co/cue/edu/miniproject/demo/controllers/VehicleController.java
@@ -1,4 +1,39 @@
 package co.cue.edu.miniproject.demo.controllers;
 
+import co.cue.edu.miniproject.demo.dtos.UserDTO;
+import co.cue.edu.miniproject.demo.dtos.VehicleDTO;
+import co.cue.edu.miniproject.demo.models.User;
+import co.cue.edu.miniproject.demo.models.Vehicle;
+import co.cue.edu.miniproject.demo.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/vehicles")
 public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping(value = "/add-user")
+    public void addVehicle(@RequestBody Vehicle vehicle) {
+        vehicleService.addVheicle(vehicle);
+    }
+
+    @DeleteMapping(value = "/delete-user/{user_id}")
+    public void removeVehicle(@PathVariable Long vehicle_id) {
+        vehicleService.remoceVehicle(vehicle_id);
+    }
+
+    @GetMapping(value = "/get-users")
+    public List<VehicleDTO> getAllVehicles() {
+        return vehicleService.getVehicle();
+    }
+
+    @GetMapping(value = "/get-user-byid/{user_id}")
+    public void VehicleById(@PathVariable Long vehicle_id){
+        vehicleService.findById(vehicle_id);
+    }
 }
